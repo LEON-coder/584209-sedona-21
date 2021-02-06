c17f9bc5d65ccfc70e2d4d99cd44082aa327e6f2
Леонид <leonid.spb2015@yandex.ru>
2021-02-06 19:01:43 +0300

    Добавление меню-бургер для мобильной версии проекта Седона

---
+++ source/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.header__menu-burger').click(function() {
+        $(this).toggleClass('active');
+        $('.header__menu-burger_list-items').slideToggle(700);
+        $('.header__menu-burger_list-items').addClass(".active-item");
+        $('.header__menu-burger').toggleClass('menu-burger--active');
+    })
+})
